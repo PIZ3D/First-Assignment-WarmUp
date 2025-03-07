944cba8281df0b3cc3c41341e9c3baf7430cd579
Parsa Ziadloo <parsaziadloo@yahoo.com>
2025-03-07 16:46:48 +0330

    completed Exercises.java

--- AP1403 - WarmUp/src/main/java/Exercises.java
+++ AP1403 - WarmUp/src/main/java/Exercises.java
@@ -4,8 +4,13 @@ public class Exercises {
         complete this function to check if the input number is prime or not
      */
     public boolean isPrime(long n) {
-        // todo
-        return false;
+        if (n <= 1) return false;
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /*
@@ -14,8 +19,40 @@ public class Exercises {
         indices start from 0, e.g. 3 is the index 4 of this sequence
         if the input is not a fibonacci number with description above, return -1
      */
+
     public long fibonacciIndex(long n) {
-        // todo
+	if(n<0)
+	{
+		return -1;
+	}
+
+    long a = 0 ;
+    long b = 1 ;
+    long index = 0 ;
+
+    if(n == 0)
+    {
+        return 0;
+    }
+
+    if(n == 1)
+    {
+        return 1;
+    }
+
+    while(b <= n)
+    {
+        long temp = b;
+        b = b + a ;
+        a = temp ;
+        index++;
+
+        if(b==n)
+        {
+            return index +1;
+        }
+    }
+
         return -1;
     }
 
@@ -38,11 +75,49 @@ public class Exercises {
         the output has to be a two-dimensional array of characters, so don't just print the triangle!
      */
     public char[][] generateTriangle(int n) {
-        // todo
-        return null;
+        if( n <= 0)
+        {
+            return new char[0][0];
+        }
+
+        char[][] triangle = new char[n][];
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            triangle[i] = new char[i+1];    // dynamically creates the array so it doesnt waste memmory (mige har radif yeki bishtar sootoon dare)
+
+            for(int j = 0 ; j <= i ; j++)
+            {
+                if( i == j || j == 0 || i == n - 1 )
+                {
+                    triangle[i][j] = '*';
+                }
+                else
+                {
+                    triangle[i][j] = ' ';
+                }
+            }
+        }
+
+        return triangle;
     }
 
-    public static void main(String[] args) {
-        // you can test your code here, but then it should be checked with test cases
+    public static void main(String[] args)
+    {
+        Exercises exercises = new Exercises();
+
+        // Test isPrime
+        System.out.println("Is 7 prime? " + exercises.isPrime(7)); // true
+        System.out.println("Is 10 prime? " + exercises.isPrime(10)); // false
+
+        // Test fibonacciIndex
+        System.out.println("Index of 3 in Fibonacci: " + exercises.fibonacciIndex(3)); // 4
+        System.out.println("Index of 7 in Fibonacci: " + exercises.fibonacciIndex(7)); // -1
+
+        // Test generateTriangle
+        char[][] triangle = exercises.generateTriangle(4);
+        for (char[] row : triangle) {
+            System.out.println(new String(row));
+        }
     }
 }
